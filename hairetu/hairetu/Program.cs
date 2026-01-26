a9ac995bdf4af81207d8fe12701e5bd4273ad668
LAPTOP-PW\fchon <f.chongi19990518.fanchon@gmail.com>
2026-01-26 21:24:12 +0900

    配列表示

--- hairetu/hairetu/Program.cs
+++ hairetu/hairetu/Program.cs
@@ -16,7 +16,7 @@ namespace hairetu
 
             string[] array = { "aaa", "bbb", "ccc", "ddd", "abc" };
 
-            if (array.Contains("aaa"))
+            if (array.Contains("abc"))
             {
                 Console.WriteLine("含んでいます");
             }
