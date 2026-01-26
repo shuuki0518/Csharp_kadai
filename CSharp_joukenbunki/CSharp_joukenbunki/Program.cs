da36fe12c191594358b8eb6cc954348cd46165d4
LAPTOP-PW\fchon <f.chongi19990518.fanchon@gmail.com>
2026-01-26 18:45:45 +0900

    条件文器の課題

---
+++ CSharp_joukenbunki/CSharp_joukenbunki/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_joukenbunki
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //奇数・偶数判定
+            Console.WriteLine("偶数・奇数判定");
+            Console.WriteLine("整数を入力してください");
+            int number = int.Parse(Console.ReadLine());
+
+            if(number%2==0)
+            {
+                Console.WriteLine("偶数");
+            }
+            else
+            {
+                Console.WriteLine("奇数");
+            }
+
+            Console.WriteLine();
+
+            //曜日判定（switch文）
+            Console.WriteLine("曜日判定");
+            Console.Write("1～7の数字を入力してください: ");
+            int day = int.Parse(Console.ReadLine());
+
+            switch (day)
+            {
+                case 1:
+                    Console.WriteLine("月曜日");
+                    break;
+                case 2:
+                    Console.WriteLine("火曜日");
+                    break;
+                case 3:
+                    Console.WriteLine("水曜日");
+                    break;
+                case 4:
+                    Console.WriteLine("木曜日");
+                    break;
+                case 5:
+                    Console.WriteLine("金曜日");
+                    break;
+                case 6:
+                    Console.WriteLine("土曜日");
+                    break;
+                case 7:
+                    Console.WriteLine("日曜日");
+                    break;
+                default:
+                    Console.WriteLine("不正な入力です");
+                    break;
+            }
+
+            Console.WriteLine();
+
+            //学生割引判定
+            Console.WriteLine("学生割引判定");
+
+            int age = 20;          // 年齢
+            bool isStudent = true; // 学生かどうか
+
+            if (age >= 18 && isStudent)
+            {
+                Console.WriteLine("学生割引が適用されます");
+            }
+            //ctrl + F5
+
+        }
+    }
+}
---
+++ CSharp_joukenbunki/CSharp_joukenbunki/betukai.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_joukenbunki
+{
+    class betukai
+    {
+
+        /*学生割引判定（入力式：コメントアウト）
+
+            Console.WriteLine("③ 学生割引判定");
+
+            Console.Write("年齢を入力してください: ");
+            int age = int.Parse(Console.ReadLine());
+
+            Console.Write("学生ですか？（true / false）: ");
+            bool isStudent = bool.Parse(Console.ReadLine());
+
+            if (age >= 18 && isStudent)
+            {
+                Console.WriteLine("学生割引が適用されます");
+            }
+            else
+            {
+                Console.WriteLine("学生割引は適用されません");
+            }*/
+
+    }
+}
