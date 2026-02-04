40456b953593e5b13da381c5025dce5e86f5e2e6
LAPTOP-PW\fchon <f.chongi19990518.fanchon@gmail.com>
2026-02-04 21:35:40 +0900

    フォーム作成2

---
+++ CSharp_FormSakusei2/CSharp_FormSakusei2/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace CSharp_FormSakusei2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtWord = new System.Windows.Forms.TextBox();
+            this.btnCheckWord = new System.Windows.Forms.Button();
+            this.lblWordResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtWord
+            // 
+            this.txtWord.Location = new System.Drawing.Point(63, 64);
+            this.txtWord.Name = "txtWord";
+            this.txtWord.Size = new System.Drawing.Size(100, 22);
+            this.txtWord.TabIndex = 0;
+            // 
+            // btnCheckWord
+            // 
+            this.btnCheckWord.Location = new System.Drawing.Point(63, 131);
+            this.btnCheckWord.Name = "btnCheckWord";
+            this.btnCheckWord.Size = new System.Drawing.Size(75, 23);
+            this.btnCheckWord.TabIndex = 1;
+            this.btnCheckWord.Text = "button1";
+            this.btnCheckWord.UseVisualStyleBackColor = true;
+            this.btnCheckWord.Click += new System.EventHandler(this.btnCheckWord_Click);
+            // 
+            // lblWordResult
+            // 
+            this.lblWordResult.AutoSize = true;
+            this.lblWordResult.Location = new System.Drawing.Point(60, 206);
+            this.lblWordResult.Name = "lblWordResult";
+            this.lblWordResult.Size = new System.Drawing.Size(37, 15);
+            this.lblWordResult.TabIndex = 2;
+            this.lblWordResult.Text = "結果";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblWordResult);
+            this.Controls.Add(this.btnCheckWord);
+            this.Controls.Add(this.txtWord);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtWord;
+        private System.Windows.Forms.Button btnCheckWord;
+        private System.Windows.Forms.Label lblWordResult;
+    }
+}
+
---
+++ CSharp_FormSakusei2/CSharp_FormSakusei2/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+
+namespace CSharp_FormSakusei2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtWord_Click(object sender, EventArgs e)
+        {
+            /*
+            //テキストボックスが空白の場合
+            if (string.IsNullOrEmpty(txtWord.Text))
+            {
+                lblWordResult.Text = "からっぽ";
+                lblWordResult.ForeColor = Color.Yellow;
+            }
+            else
+            {
+                CheckWord(txtWord.Text);
+            }
+            */
+        }
+
+        //入力内容チェックメソッド
+        private void CheckWord(string text)
+        {
+            bool hasMountain = text.Contains("山");
+            bool hasSea = text.Contains("海");
+
+            if (hasMountain && hasSea)
+            {
+                lblWordResult.Text = "どちらも含まれている";
+                lblWordResult.ForeColor = Color.Red;
+            }
+            else if (hasMountain)
+            {
+                lblWordResult.Text = "山が含まれている";
+                lblWordResult.ForeColor = Color.Green;
+            }
+            else if (hasSea)
+            {
+                lblWordResult.Text = "海が含まれている";
+                lblWordResult.ForeColor = Color.Blue;
+            }
+            else
+            {
+                lblWordResult.Text = "どちらも含まれてない";
+                lblWordResult.ForeColor = Color.Black;
+            }
+        }
+
+        private void btnCheckWord_Click(object sender, EventArgs e)
+        {
+            //テキストボックスが空白の場合
+            if (string.IsNullOrEmpty(txtWord.Text))
+            {
+                lblWordResult.Text = "からっぽ";
+                lblWordResult.ForeColor = Color.Yellow;
+            }
+            else
+            {
+                CheckWord(txtWord.Text);
+            }
+        }
+    }
+}
